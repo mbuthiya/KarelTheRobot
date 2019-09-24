ce1140c57d4f4daf0bc012189ccd5de5e18bfbd8
James <jamesmuriukimbuthia@gmail.com>
2019-09-24 14:46:34 +0300

    Create fillColumn method
    created a method fillColumn that fills the column with stones and stops when
    it hits a wall

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -11,91 +11,24 @@
 import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
-
-	// You fill in this part
+	
 	public void run() {
-//		Start position
 		turnLeft();
-		
-//		First Column
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		
-//		Move to next Column
-		turnRight();
-		moves(4);
-		turnRight();
-		
-//		Second column
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-// Moves to the next Column
-		turnLeft();
-		moves(4);
-		turnLeft();
-		
-//		Third Column
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-// Moves to the next Column
-		turnRight();
-		moves(4);
-		turnRight();
-		
-//		Fourth Column
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		fillColumnSpace();
-		moves(1);
-		
-//		Final Position
-		turnLeft();
-		
+		fillColumn();
 		
 	}
 	
-	
-	
-	public void fillColumnSpace() {
-		if(noBeepersPresent()) {
-			putBeeper();
-		}
-		
-	}
-	
-
-	
-	public void moves(int steps) {
-		if(frontIsClear()) {
-			for(int i = 0; i<steps; i++) {
+	public void fillColumn() {
+		while(frontIsClear()) {
+			if(noBeepersPresent()) {
+				putBeeper();
+			}else {
 				move();
 			}
 		}
-	
+		if(noBeepersPresent()) {
+			putBeeper();
+		}
 	}
-
+	
 }
